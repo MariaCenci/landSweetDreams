05751266f85f64e3056c5f227eaa22b237fd0d13
Maria Fernanda Cenci <mariafcenci@gmail.com>
2024-08-06 20:10:05 -0300

    chore: added script fontawesome to html

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import  {Link}  from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-            <Link to='/' className='navbar-logo'>
-            SweetDreams
-            </Link>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            <img src="/favicon.png" width={"22px"} /> SweetDreams
+          </Link>
+          <div className="menu-icon">
+          <i className="fa-solid fa-bars" />
+          </div>
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
