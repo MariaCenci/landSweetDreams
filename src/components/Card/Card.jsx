fa5c833ad78fe2d38da84de578d875292b4f0535
Maria Fernanda Cenci <mariafcenci@gmail.com>
2024-08-09 10:34:26 -0300

    chore: added style to cards, added style to products section

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,15 +3,17 @@ import "./card.scss";
 import Button from "../Button/Button.jsx";
 import PriceTag from "../PriceTag/PriceTag.jsx";
 
-const Card = ({ title, src, text }) => {
+const Card = ({ title, src, text, price }) => {
   return (
     <>
       <div className="card-item">
-      <PriceTag price="$18"/>
+        <PriceTag price={price} />
         <img className="card-item-img" src={src} alt="Bakery item" />
-        <h2 className="card-item-title">{title}</h2>
-        <p className="card-item-text">{text}</p>
-        <Button className="card-item" label="Buy now" />
+        <div className="card-info-container">
+          <h2 className="card-item-title">{title}</h2>
+          <p className="card-item-text">{text}</p>
+          <Button className="card-item" label="Buy now" />
+        </div>
       </div>
     </>
   );
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,19 +1,54 @@
 import React from "react";
 import Card from "../../components/Card/Card";
-//import PriceTag from "../../components/PriceTag/PriceTag";
+import "./products.scss";
 
 const Products = () => {
   return (
     <>
-      <div className="cards-container">
-        <div className="cards-wrapper">
-          <div className="section-title">
-            <h1>Cakes</h1>
-          </div>
-          
-          <Card title="Cheesecake"  src="./images/cheesecake.jpg" text="Our cheesecake" />
+      <section id="products-section">
+        <h1 className="title-section">Cakes</h1>
+        <div className="cards-container">
+          <Card
+            title="Cheesecake"
+            src="./images/cheesecake.jpg"
+            text="Creamy and smooth, our classic cheesecake is a slice of heaven in every bite."
+            price="$30"
+          />
+
+          <Card
+            title="Citrus cake"
+            src="./images/citrus.jpg"
+            text="Bright and zesty, our citrus cake offers a refreshing burst of lemon and orange flavors."
+            price="$25"
+          />
+
+          <Card
+            title="B&W cake"
+            src="./images/b&w.jpg"
+            text="Indulge in the perfect balance of rich chocolate and velvety vanilla in our Black & White cake."
+            price="$20"
+          />
+
+          <Card
+            title="Chocolate cake"
+            src="./images/chocolatecake.jpg"
+            text="Decadent and moist, our chocolate cake is a true delight for chocolate lovers."
+            price="$18"
+          />
+          <Card
+            title="Brownies"
+            src="./images/brownies.jpg"
+            text="Fudgy, dense, and irresistibly chocolatey, our brownies are a treat you can't miss."
+            price="$15"
+          />
+          <Card
+            title="Red berries cake"
+            src="./images/redcake.jpg"
+            text="Sweet and tangy, our red berries cake is bursting with the flavor of fresh seasonal berries."
+            price="$28"
+          />
         </div>
-      </div>
+      </section>
     </>
   );
 };
