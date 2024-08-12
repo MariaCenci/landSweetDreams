1ddbea79c34080d9c46fb17b822744b51180c945
Maria Fernanda Cenci <mariafcenci@gmail.com>
2024-08-12 11:17:25 -0300

    feat: implemented about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
+import "./about.scss";
 
 const About = () => {
   return (
     <>
-     
+      <section id="about-section">
+        <h1 className="title-section">About us</h1>
+        <div className="about-container">
+          <div className="img-about-container">
+            <img
+              className="img-about-section"
+              src="./images/about.webp"
+              alt="kitchen utensils"
+            />
+          </div>
+          <div className="text-about-container">
+            <p className="text-about">
+              Our passion for sweets began in our home kitchen, where every
+              recipe was an expression of love and dedication. Over time, this
+              love for baking grew into a business that values homemade flavors,
+              fresh ingredients, and the joy of sharing sweet moments.
+            </p>
+            <div className="icons-about-container">
+              <div className="icon-about-wrapper">
+                <span className="icon-about">
+                  <img src="./icons/iconPeople.svg" alt="" />
+                </span>
+                <span className="icon-about-text">
+                  {" "}
+                  <p>
+                    <strong>5.000+</strong> <br />
+                    customers
+                  </p>
+                </span>
+              </div>
+              <div className="icon-about-wrapper">
+                <span className="icon-about">
+                  <img src="./icons/iconNumbers.svg" alt="" />
+                </span>
+                <span className="icon-about-text">
+                  {" "}
+                  <p>
+                    <strong>10.000+</strong> <br /> sells
+                  </p>
+                </span>
+              </div>
+             
+              <div className="icon-about-wrapper">
+                <span className="icon-about">
+                  <img src="./icons/iconYears.svg" alt="" />
+                </span>
+                <span className="icon-about-text">
+                  {" "}
+                  <p>
+                    <strong>20+</strong> <br /> years of experience
+                  </p>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
